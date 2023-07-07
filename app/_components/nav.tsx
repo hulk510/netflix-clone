13d0939cc539bbfdfc094f7a65e8410ca50628a8
hulk510 <dorian.51069@gmail.com>
2023-07-08 01:00:09 +0900

    modify img to Image

--- app/_components/nav.tsx
+++ app/_components/nav.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import styles from './nav.module.css';
 
@@ -22,15 +23,19 @@ export const Nav = () => {
 
   return (
     <div className={`${show ? styles['nav-black'] : styles.nav}`}>
-      <img
+      <Image
         className={styles['nav-logo']}
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         alt="Netflix Logo"
+        width="80"
+        height="30"
       />
-      <img
+      <Image
         className={styles['nav-avater']}
         src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
         alt="Avatar"
+        width="30"
+        height="30"
       />
     </div>
   );
