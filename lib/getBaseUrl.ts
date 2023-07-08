0ec3616ab4c9e38f6a6930f069dde10fcd68d0db
hulk510 <dorian.51069@gmail.com>
2023-07-08 23:26:46 +0900

    url変更

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,8 +1,7 @@
 import { cache } from 'react';
 
-export const getBaseUrl = cache(() => {
-  console.log(process.env.VERCEL_URL);
+export const getBaseUrl = cache(() =>
   process.env.VERCEL_URL
-    ? `https://asddf/`
-    : `http://localhost:${process.env.PORT ?? 3000}`;
-});
+    ? `https://${process.env.VERCEL_URL}/`
+    : `http://localhost:${process.env.PORT ?? 3000}`
+);
